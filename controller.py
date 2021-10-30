c00032145b71196606566d49a6887f28a51bb7eb
Desmond <poonweijun@gmail.com>
2021-10-30 21:02:19 +0800

    Classes defined properly

--- controller.py
+++ controller.py
@@ -3,45 +3,86 @@ import entity as E
 
 class Session:
     user = None
-    args = ()
-    kwargs = {}
+    context = None
 
-    def __init__(self, user, *args, **kwargs):
-        self.user = user
-        self.args = args
-        self.kwargs = kwargs
+    @staticmethod
+    def set_user(user):
+        Session.user = user
 
+    @staticmethod
+    def get_user():
+        return Session.user
 
-class LoginController:
-    def checkLoginInput(self, email, password):
-        # check if password is empty
-        if email == "" or password == "":
-            return False
-        return E.User.validate_login(email, password)
+    @staticmethod
+    def set_context(context):
+        Session.context = context
+
+    @staticmethod
+    def get_context():
+        return Session.context
+
+# class LoginController:
+#     def checkLoginInput(self, email, password):
+#         # check if password is empty
+#         if email == "" or password == "":
+#             return False
+#         return E.User.validate_login(email, password)
 
 
 # REMINDER FOR DESMOND: PASS IN QUERYING USER OBJECT TO CHECK VALIDITY
 class UserController:
-    @staticmethod
-    def retrieve_patient_prescriptions(patient_id):
-        user_entity = E.User()
-        user_entity.set_id(patient_id)
-        return user_entity.retrieve_user_prescriptions()
+    # @staticmethod
+    # def retrieve_patient_prescriptions(patient_id):
+    #     # user_entity = E.User()
+    #     # user_entity.set_id(patient_id)
+    #     # return user_entity.retrieve_user_prescriptions()
 
     @staticmethod
     def retrieve_all_users():
-        return E.User.retrieve_all_users()
+        # return E.User.retrieve_all_users()
+        return E.UserEntity().retrieve_all()
         
     @staticmethod
     def retrieve_user(user_id):
-        return E.User().retrieve_user(user_id)
+        # return E.User().retrieve_user(user_id)
+        return E.UserEntity().retrieve_one(user_id)
+
+    @staticmethod
+    def login(email, password):
+        if email == "" or password == "":
+            return False
+        return E.UserEntity().validate_login(email, password)
 
 
 class PrescriptionController:
+    e = E.PrescriptionEntity()
+
     @staticmethod
     def retrieve_all_prescriptions():
-        return E.Prescription.get_all_prescriptions()
+        # return E.Prescription.get_all_prescriptions()
+        return PrescriptionController.e.retrieve_all()
 
     @staticmethod
     def retrieve_prescription(code):
-        return E.Prescription().get_prescription(code)
+        # return E.Prescription().retrieve_prescription(code)
+        return PrescriptionController.e.retrieve_one(code)
+
+    @staticmethod
+    def retrieve_patient_prescriptions(patient_id):
+        return PrescriptionController.e.retrieve_by_fk(patient_id)
+
+
+class MedicineController:
+    e = E.MedicineEntity()
+
+    @staticmethod
+    def retrieve_medicine(medicine_id):
+        return MedicineController.e.retrieve_one(medicine_id)
+
+    @staticmethod
+    def retrieve_all_medicines():
+        return MedicineController.e.retrieve_all()
+
+    # @staticmethod
+    # def retrieve_prescription_medicines(prescription_id):
+    #     return MedicineController.e.
--- entity.py
+++ entity.py
@@ -1,14 +1,18 @@
 import db_helper
 
 
+# id | user_type_id | name | email | address | phone_number | user_type | password #
 class User:
-    id = -1
-    user_type_id = ''
-    name = ''
-    email = ''
-    address = ''
-    phone_number = ''
-    user_type = ''
+    DATABASE = db_helper.db_helper()
+
+    def __init__(self, id, user_type_id, name, email, address, phone_number, user_type):
+        self.id = id
+        self.user_type_id = user_type_id
+        self.name = name
+        self.email = email
+        self.address = address
+        self.phone_number = phone_number
+        self.user_type = user_type
 
     def set_id(self, user_type_id):
         self.user_type_id = user_type_id
@@ -26,8 +30,8 @@ class User:
     def validate_login(email, password):
         validation_query = f"SELECT * FROM USERS WHERE email = '{email}' AND PASSWORD = '{password}'"
         # validate input
-        db_obj = db_helper.db_helper()  # call helper object
-        result = db_obj.query_db(validation_query)  # retrieve query results
+        # db_obj = db_helper.db_helper()  # call helper object
+        result = User.DATABASE.query_db(validation_query)  # retrieve query results
         if len(result) == 1:
             r = result[0]
             u = User()
@@ -35,40 +39,22 @@ class User:
             return u
         return False
 
-    def retrieveUserInfo(self, userNo):  # need to pass in user email not userID?
-        listOfUsers = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]]
-        return listOfUsers
-
-    def retrieveSpecificUser(self, userNo):
-        # select lineEdit from database
-        listofdetails = [1, 2, 3, 4, 5]
-        return listofdetails
-
     def retrieve_user(self, user_id):
         query = f"SELECT * FROM USERS WHERE user_type_id = '{user_id}'"
-        db_obj = db_helper.db_helper()
-        results = db_obj.query_db(query)
+        # db_obj = db_helper.db_helper()
+        results = User.DATABASE.query_db(query)
         if len(results) > 0:
             r = results[0]
             self.set_values(r[0], r[1], r[2], r[3], r[4], r[5], r[6])
             return self
         return False
 
-    # @staticmethod
-    # def retrieve_all_users(self):
-    #     query = f"SELECT * FROM USERS"
-    #     db_obj = db_helper.db_helper()
-    #     results = db_obj.query_db(query)
-    #     for user in results:
-    #         u = User()
-    #         u.set_id()
-
     def retrieve_user_prescriptions(self):
         prescriptions = []
         print(self.user_type_id)
         query = f"SELECT * FROM PRESCRIPTION WHERE patient_id = '{self.user_type_id}'"
-        db_obj = db_helper.db_helper()  # call helper object
-        results = db_obj.query_db(query)
+        # db_obj = db_helper.db_helper()  # call helper object
+        results = User.DATABASE.query_db(query)
         for prescription in results:
             print(f'presciption: {prescription}')
             p = Prescription()
@@ -80,8 +66,8 @@ class User:
     @staticmethod
     def retrieve_all_users():
         query = f"SELECT * FROM USERS"
-        db_obj = db_helper.db_helper()
-        result = db_obj.query_db(query)
+        # db_obj = db_helper.db_helper()
+        result = User.DATABASE.query_db(query)
         users = []
         for user in result:
             u = User()
@@ -90,46 +76,57 @@ class User:
         return users
 
     def __str__(self):
-        return f'Patient ID: {self.user_type_id}'
+        return f'User: {{\n' \
+               f'\t\'id\': {self.id},\n' \
+               f'\t\'user_type_id\': \'{self.user_type_id}\',\n' \
+               f'\t\'name\': \'{self.name}\',\n' \
+               f'\t\'email\': \'{self.email}\',\n' \
+               f'\t\'address\': \'{self.address}\',\n' \
+               f'\t\'phone_number\': \'{self.phone_number}\',\n' \
+               f'\t\'user_type\': \'{self.user_type}\',\n' \
+               f'}},'
 
-
+# date | string_code | prescription_id | patient_id | doctor_id | status | prescription_detail #
 class Prescription:
-    date = ''
-    code = ''
-    prescription_id = ''
-    patient_id = ''
-    doctor_id = ''
-    status = None
-    prescription_details = ''
+    DATABASE = db_helper.db_helper()
+
+    def __init__(self, date, code, prescription_id, patient_id, doctor_id, status, prescription_detail):
+        self.date = date
+        self.code = code
+        self.prescription_id = prescription_id
+        self.patient_id = patient_id
+        self.doctor_id = doctor_id
+        self.status = status
+        self.prescription_detail = prescription_detail
 
     def set_id(self, prescription_id):
         query = f"SELECT * FROM PRESCRIPTION WHERE prescription_id = '{prescription_id}'"
-        db_obj = db_helper.db_helper()
-        result = db_obj.query_db(query)
+        # db_obj = db_helper.db_helper()
+        result = Prescription.DATABASE.query_db(query)
         print(result)
         if len(result) > 0:
             r = result[0]
             self.set_values(r[0], r[1], r[2], r[3], r[4], r[5], r[6])
 
-    def set_values(self, date, code, prescription_id, patient_id, doctor_id, status, prescription_details):
+    def set_values(self, date, code, prescription_id, patient_id, doctor_id, status, prescription_detail):
         self.date = date
         self.code = code
         self.prescription_id = prescription_id
         self.patient_id = patient_id
         self.doctor_id = doctor_id
         self.set_status_string(status)
-        self.prescription_details = prescription_details
+        self.prescription_detail = prescription_detail
 
     def retrieve_record(self):
-        return self.prescription_details
+        return self.prescription_detail
 
-    def set_status_string(self, status):
-        self.status = 'Collected' if status == 1 else 'Not collected'
+    def get_status_string(self):
+        return 'Collected' if self.status == 1 else 'Not collected'
 
-    def get_prescription(self, code):
+    def retrieve_prescription(self, code):
         query = f"SELECT * FROM PRESCRIPTION WHERE string_code = '{code}'"
-        db_obj = db_helper.db_helper()
-        result = db_obj.query_db(query)
+        # db_obj = db_helper.db_helper()
+        result = Prescription.DATABASE.query_db(query)
         if len(result) > 0:
             r = result[0]
             self.set_values(r[0], r[1], r[2], r[3], r[4], r[5], r[6])
@@ -139,8 +136,8 @@ class Prescription:
     @staticmethod
     def get_all_prescriptions():
         query = f"SELECT * FROM PRESCRIPTION"
-        db_obj = db_helper.db_helper()
-        result = db_obj.query_db(query)
+        # db_obj = db_helper.db_helper()
+        result = Prescription.DATABASE.query_db(query)
         items = []
         for item in result:
             p = Prescription()
@@ -150,9 +147,120 @@ class Prescription:
         return items
 
     def __str__(self):
-        return f'Prescription: {self.prescription_id}'
+        return f'Prescription: {{\n' \
+               f'\t\'date\': {self.date},\n' \
+               f'\t\'code\': \'{self.code}\',\n' \
+               f'\t\'prescription_id\': \'{self.prescription_id}\',\n' \
+               f'\t\'patient_id\': \'{self.patient_id}\',\n' \
+               f'\t\'doctor_id\': \'{self.doctor_id}\',\n' \
+               f'\t\'status\': \'{self.status}\',\n' \
+               f'\t\'prescription_detail\': \'{self.prescription_detail}\',\n' \
+               f'}},'
 
 
+# id | medicine_name #
 class Medicine:
-    id = -1
-    name = ''
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __str__(self):
+        return f'Medicine: {{\n' \
+               f'\t\'id\': {self.id},\n' \
+               f'\t\'name\': \'{self.name}\',\n' \
+               f'}},'
+
+
+class ObjectEntity:
+    DATABASE = db_helper.db_helper()
+
+    def retrieve_one(self, query):
+        # result = self.retrieve_all(query)
+        result = ObjectEntity.DATABASE.query_db(query)
+        if len(result) > 0:
+            return result[0]
+        return []
+
+    def retrieve_all(self, query):
+        return ObjectEntity.DATABASE.query_db(query)
+
+
+class UserEntity(ObjectEntity):
+    def retrieve_one(self, object_id):
+        query = f"SELECT * FROM USERS WHERE user_type_id = '{object_id}'"
+        result = super(UserEntity, self).retrieve_one(query)
+        # print(f'result: {result}')
+        if len(result) > 0:
+            return User(result[0], result[1], result[2], result[3], result[4], result[5], result[6])
+        return result
+        # results = ObjectEntity.DATABASE.query_db(query)
+        # if len(results) > 0:
+        #     r = results[0]
+        #     user = User(r[0], r[1], r[2], r[3], r[4], r[5], r[6])
+        #     return user
+        # return False
+
+    def retrieve_all(self, query=None):
+        query = f"SELECT * FROM USERS"
+        result = super(UserEntity, self).retrieve_all(query)
+        users = []
+        for r in result:
+            user = User(r[0], r[1], r[2], r[3], r[4], r[5], r[6])
+            users.append(user)
+        return users
+        # result = ObjectEntity.DATABASE.query_db(query)
+
+    def validate_login(self, email, password):
+        validation_query = f"SELECT * FROM USERS WHERE email = '{email}' AND PASSWORD = '{password}'"
+        result = ObjectEntity.DATABASE.query_db(validation_query)
+        if len(result) > 0:
+            r = result[0]
+            return User(r[0], r[1], r[2], r[3], r[4], r[5], r[6])
+        return result
+        # return self.retrieve_one(validation_query)
+
+
+class PrescriptionEntity(ObjectEntity):
+    def retrieve_one(self, object_id):
+        query = f"SELECT * FROM PRESCRIPTION WHERE string_code = '{object_id}'"
+        result = super(PrescriptionEntity, self).retrieve_one(query)
+        if len(result) > 0:
+            return Prescription(result[0], result[1], result[2], result[3], result[4], result[5], result[6])
+        return result
+
+    def retrieve_all(self, query=None):
+        if query is None:
+            query = f"SELECT * FROM PRESCRIPTION"
+        result = super(PrescriptionEntity, self).retrieve_all(query)
+        prescriptions = []
+        for r in result:
+            prescription = Prescription(r[0], r[1], r[2], r[3], r[4], r[5], r[6])
+            prescriptions.append(prescription)
+        return prescriptions
+
+    def retrieve_by_fk(self, fk):
+        query = f"SELECT * FROM PRESCRIPTION WHERE patient_id = '{fk}'"
+        print(f'query: {fk}')
+        return self.retrieve_all(query)
+
+
+class MedicineEntity(ObjectEntity):
+    def retrieve_one(self, object_id):
+        query = f"SELECT * FROM MEDICINE WHERE id = '{object_id}'"
+        return super(MedicineEntity, self).retrieve_one(query)
+        # if len(result) > 0:
+        #     return Medicine(result[0], result[1])
+        # return result
+
+    def retrieve_all(self, query=None):
+        query = f"SELECT * FROM MEDICINE"
+        # result = ObjectEntity.DATABASE.query_db(query)
+        result = super(MedicineEntity, self).retrieve_all(query)
+        medicines = []
+        for r in result:
+            medicine = Medicine(r[0], r[1])
+            medicines.append(medicine)
+        return medicines
+
+    # def retrieve_by_fk(self, fk):
+        query = f"-- SELECT * FROM MEDICINE WHERE prescription_id"
