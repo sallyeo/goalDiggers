76071a2d8d08441040d6136efb3b5700a3d91f5f
sallyeo <sallyeo.sy@gmail.com>
2021-11-02 00:23:31 +0800

    edited DoctorViewPatient btn removed logout btn from doctorViewPrescription.ui

--- main.py
+++ main.py
@@ -388,6 +388,7 @@ class DoctorViewPatient(ViewUser):
     def __init__(self, column_sizes=None):
         super(DoctorViewPatient, self).__init__('doctorViewPatient.ui')
         widget.setFixedSize(730, 650)
+        self.table.verticalHeader().setVisible(False)   # remove most left column
         if column_sizes is None:
             column_sizes = [50, 100, 100, 100, 100]
         for i in range(len(column_sizes)):
